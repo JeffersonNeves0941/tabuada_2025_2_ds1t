8092b9a9a3b966815087b228974143f54b17c78f
CELSO MARCOS FURTADO <sn1006137@CFP127ALUNO.LOCAL>
2025-11-03 16:59:55 -0300

    Tabuada modo texto

---
+++ src/br/senai/sp/jandira/tabuada/Tabuada.java
@@ -0,0 +1,63 @@
+package br.senai.sp.jandira.tabuada;
+
+import java.util.Scanner;
+
+public class Tabuada {
+
+    public int multiplicando;
+    public int multiplicadorInicial;
+    public int multiplicadorFinal;
+    public String[] tabuada;
+
+    public void coletarDados(){
+        Scanner leitor = new Scanner(System.in);
+
+        System.out.print("Qual é o valor do multiplicando? ");
+        multiplicando = leitor.nextInt();
+
+        System.out.print("Qual é o valor do multiplicador inicial? ");
+        multiplicadorInicial = leitor.nextInt();
+
+        System.out.print("Qual é o valor do multiplicador final? ");
+        multiplicadorFinal = leitor.nextInt();
+
+        calcularTabuada();
+
+    }
+
+    public void calcularTabuada(){
+        int apoio = 0;
+
+        if (multiplicadorFinal < multiplicadorInicial){
+            apoio = multiplicadorFinal;
+            multiplicadorFinal = multiplicadorInicial;
+            multiplicadorInicial = apoio;
+        }
+
+        int tamanho = multiplicadorFinal - multiplicadorInicial + 1;
+        tabuada = new String[tamanho];
+
+        int i = 0;
+        while (i < tamanho){
+            int produto = multiplicando * multiplicadorInicial;
+            tabuada[i] = multiplicando + " X " + multiplicadorInicial + " = " + produto;
+            multiplicadorInicial = multiplicadorInicial + 1;
+            i =  i + 1;
+        }
+
+        exibirTabuada();
+
+    }
+
+    public void exibirTabuada(){
+        System.out.println("Resultado da sua tabuada");
+
+        int i = 0;
+        while (i < tabuada.length){
+            System.out.println(tabuada[i]);
+            i++; // i = i + 1
+        }
+
+    }
+
+}
---
+++ src/br/senai/sp/jandira/tabuada/TabuadaMain.java
@@ -0,0 +1,9 @@
+package br.senai.sp.jandira.tabuada;
+
+public class TabuadaMain {
+    public static void main(String[] args) {
+        System.out.println("Tabuada main");
+        Tabuada tabuada = new Tabuada();
+        tabuada.coletarDados();
+    }
+}
